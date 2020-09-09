805276047e4397d09a6e9e69e54849ecc00ab446
Philipp Schemel <philippschemel@live.com>
2020-09-09 23:32:31 +0200

    Try to solve puzzle

--- 2019/day3/part1.js
+++ 2019/day3/part1.js
@@ -1,8 +1,35 @@
 var fs = require('fs')
+var os = require('os')
 
 fs.readFile('input.txt', 'utf8', function(err, data) {
-    let sequence = data.split(',')
+    let inputs = data.split(os.EOL).map(input => input.split(','))
 
-    console.log('sequence', sequence)
+    let coordinates = []
+
+    inputs.forEach((input, i) => {
+
+        let pointer = { x: 0, y: 0 }
+
+        coordinates[i] = [pointer]
+
+        input.forEach(command => {
+            console.log(command, pointer)
+
+            const direction = command.substr(0, 1)
+            const amount = command.substr(1, command.length)
+            const axis = (direction === 'R' || direction === 'L') ? 'x' : 'y'
+            const isPositive = direction === 'R' || direction === 'U'
+
+            const startingMover = coordinates[i][coordinates[i].length - 1][axis]
+
+            for (let mover = startingMover; mover < startingMover + amount; mover++) {
+                pointer[axis] = isPositive ? startingMover + mover : startingMover - mover
+                coordinates[i].push({x: pointer.x, y: pointer.y})
+            }
+
+            console.log(coordinates[i])
+
+        })
+    })
 
 })
