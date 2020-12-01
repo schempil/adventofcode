099297417e8b3a06dad8c00c33b1a85c58976518
Philipp Schemel <philippschemel@live.com>
2020-12-01 23:12:37 +0100

    Read and parse input to array

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -2,10 +2,32 @@ package day1
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"strings"
 )
 
 func Solve() {
 
 	fmt.Println("Hello AventOfCode 2020 - Day #1")
+
+	filepath, pathError := filepath.Abs("./day1/input.txt")
+
+	if pathError != nil {
+		log.Fatal(pathError)
+	}
+
+	content, readFileError := ioutil.ReadFile(filepath)
+
+	if readFileError != nil {
+		log.Fatal(readFileError)
+	}
+
+	text := string(content)
+
+	array := strings.Split(text, "\n")
+
+	fmt.Println(len(array))
 
 }
