cfe6057ce90181bd39318d0f5fa4c7be6b3f1e06
Philipp Schemel <philippschemel@live.com>
2020-12-02 00:10:48 +0100

    Refactoring

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -3,50 +3,78 @@ package day1
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func Solve() {
-
-	fmt.Println("Hello AventOfCode 2020 - Day #1")
-
-	filepath, _ := filepath.Abs("./day1/input.txt")
-
-	content, readFileError := ioutil.ReadFile(filepath)
-
-	if readFileError != nil {
-		log.Fatal(readFileError)
-	}
-
+	absolutePath, _ := filepath.Abs("./day1/input.txt")
+	content, _ := ioutil.ReadFile(absolutePath)
 	text := string(content)
+	input := strings.Split(text, "\n")
 
-	array := strings.Split(text, "\n")
+	fmt.Println("Solution Day 1 - Part 1:", findTuple(input))
+	fmt.Println("Solution Day 1 - Part 2:", findThirdValueToMatchTupleTo2020(input))
+}
 
-	for _, element := range array {
+func findTuple(input []string) int {
+	value1, _ := strconv.Atoi(input[0])
+	reducedInput := input[1:]
 
-		value1, _ := strconv.Atoi(element)
+	for _, element := range reducedInput {
+		value2, _ := strconv.Atoi(element)
 
-		for _, element2 := range array {
-
-			value2, _ := strconv.Atoi(element2)
-
-			if value1+value2 == 2020 {
-				fmt.Println("Solution Part1:", value1*value2)
-			}
-
-			for _, element3 := range array {
-
-				value3, _ := strconv.Atoi(element3)
-
-				if value1+value2+value3 == 2020 {
-					fmt.Println("Solution Part2:", value1*value2*value3)
-				}
-			}
+		if value1+value2 == 2020 {
+			return value1 * value2
 		}
 
 	}
 
+	return findTuple(reducedInput)
+}
+
+type tuple struct {
+	val1 int
+	val2 int
+}
+
+func findTuplesSumLowerThan2020(input []string) []tuple {
+	var possibleStarts []tuple
+
+	if len(input) > 1 {
+		value1, _ := strconv.Atoi(input[0])
+		reducedInput := input[1:]
+
+		for _, element := range reducedInput {
+			value2, _ := strconv.Atoi(element)
+
+			if value1+value2 < 2020 {
+				possibleStarts = append(possibleStarts, tuple{value1, value2})
+			}
+
+		}
+
+		possibleStarts = append(possibleStarts, findTuplesSumLowerThan2020(reducedInput)...)
+	}
+
+	return possibleStarts
+}
+
+func findThirdValueToMatchTupleTo2020(input []string) int {
+	value1, _ := strconv.Atoi(input[0])
+	reducedInput := input[1:]
+
+	tuplesSumLowerThan2020 := findTuplesSumLowerThan2020(input)
+
+	for _, tuple := range tuplesSumLowerThan2020 {
+		value2 := tuple.val1 + tuple.val2
+
+		if value1+value2 == 2020 {
+			return value1 * tuple.val1 * tuple.val2
+		}
+
+	}
+
+	return findThirdValueToMatchTupleTo2020(reducedInput)
 }
