6a377558845fe34b42df153c1ad53c163392a3a7
Philipp Schemel <philippschemel@live.com>
2020-12-01 23:20:11 +0100

    Solve part1 of day1

--- 2020/day1/day1.go
+++ 2020/day1/day1.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -12,11 +13,7 @@ func Solve() {
 
 	fmt.Println("Hello AventOfCode 2020 - Day #1")
 
-	filepath, pathError := filepath.Abs("./day1/input.txt")
-
-	if pathError != nil {
-		log.Fatal(pathError)
-	}
+	filepath, _ := filepath.Abs("./day1/input.txt")
 
 	content, readFileError := ioutil.ReadFile(filepath)
 
@@ -28,6 +25,19 @@ func Solve() {
 
 	array := strings.Split(text, "\n")
 
-	fmt.Println(len(array))
+	for _, element := range array {
+
+		value1, _ := strconv.Atoi(element)
+
+		for _, element2 := range array {
+
+			value2, _ := strconv.Atoi(element2)
+
+			if value1+value2 == 2020 {
+				fmt.Println("Solution:", value1*value2)
+			}
+		}
+
+	}
 
 }
