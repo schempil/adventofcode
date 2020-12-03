acb61dd3157e6b1b0bfc6a392ab41255c2b00017
Philipp Schemel <philippschemel@live.com>
2020-12-03 17:35:23 +0100

    Solve day3 part1

--- 2020/day3/day3.go
+++ 2020/day3/day3.go
@@ -13,8 +13,39 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println(inputs)
+	trees := travelRouteWithRationalNumbers(inputs)
 
-	fmt.Println("Solution Day 3 - Part 1:")
-	fmt.Println("Solution Day 3 - Part 2:")
+	fmt.Println("Solution Day 3 - Part 1:", trees)
+	fmt.Println("Solution Day 3 - Part 2:", "NOT SOLVED YET")
+}
+
+type position struct {
+	x int
+	y int
+}
+
+func travelRouteWithRationalNumbers(inputs []string) int {
+
+	currentPosition := position{0, 0}
+	treeCount := 0
+
+	for index, _ := range inputs {
+		currentPosition = position{currentPosition.x + 3, currentPosition.y + 1}
+
+		if index+1 >= len(inputs)-1 {
+			break
+		}
+
+		inputParts := strings.Split(inputs[currentPosition.y], "")
+
+		if getSymbolAtPositionInRepeatingPattern(inputParts, currentPosition.x) == "#" {
+			treeCount++
+		}
+	}
+
+	return treeCount
+}
+
+func getSymbolAtPositionInRepeatingPattern(inputParts []string, positionX int) string {
+	return inputParts[positionX%len(inputParts)]
 }
