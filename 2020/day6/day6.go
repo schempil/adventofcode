960a724c759f521ad899f80a2c4b61699f7c32c5
Philipp Schemel <philippschemel@live.com>
2020-12-07 01:15:25 +0100

    Solve day6 part1

--- 2020/day6/day6.go
+++ 2020/day6/day6.go
@@ -1,4 +1,4 @@
-package day5
+package day6
 
 import (
 	"fmt"
@@ -11,8 +11,35 @@ func Solve() {
 	absolutePath, _ := filepath.Abs("./day6/input.txt")
 	content, _ := ioutil.ReadFile(absolutePath)
 	text := string(content)
-	inputs := strings.Split(text, "\n")
+	inputs := strings.Split(text, "\n\n")
 
-	fmt.Println("Solution Day 6 - Part 1:", inputs[0])
+	fmt.Println("Solution Day 6 - Part 1:", getAnsweredYesSum(inputs))
 	fmt.Println("Solution Day 6 - Part 2:")
+}
+
+func getAnsweredYesSum(inputs []string) int {
+
+	sum := 0
+
+	for _, input := range inputs {
+		sum += answeredYesPerGroup(input)
+	}
+
+	return sum
+}
+
+func answeredYesPerGroup(input string) int {
+
+	persons := strings.Split(input, "\n")
+	answeredQuestionsMap := make(map[string]bool)
+
+	for _, person := range persons {
+		answers := strings.Split(person, "")
+
+		for _, answer := range answers {
+			answeredQuestionsMap[answer] = true
+		}
+	}
+
+	return len(answeredQuestionsMap)
 }
