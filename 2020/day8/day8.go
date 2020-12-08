be1c706472fcd1ec67bdac8e92a1912ccd974764
Philipp Schemel <philippschemel@live.com>
2020-12-08 16:46:21 +0100

    Solve day8 part1

--- 2020/day8/day8.go
+++ 2020/day8/day8.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +14,86 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 8 - Part 1:", inputs)
-	fmt.Println("Solution Day 8 - Part 2:")
+	acc := 0
+	commands := parseCommands(inputs)
+
+	executeCommands(commands, &acc)
+
+	fmt.Println("Solution Day 8 - Part 1:", acc)
+	fmt.Println("Solution Day 8 - Part 2:", inputs[0])
+}
+
+type command struct {
+	id       int
+	name     string
+	operator string
+	value    int
+}
+
+func executeCommands(commands []command, acc *int) {
+
+	runMap := make(map[int]bool)
+
+	for index := 0; index < len(commands); {
+		actualCommand := commands[index]
+
+		if runMap[actualCommand.id] == true {
+			break
+		}
+
+		runMap[actualCommand.id] = true
+
+		if actualCommand.name == "nop" {
+			index++
+		}
+
+		if actualCommand.name == "acc" {
+			updateAccumulator(acc, actualCommand.operator, actualCommand.value)
+			index++
+		}
+
+		if actualCommand.name == "jmp" {
+			jump(&index, actualCommand.operator, actualCommand.value)
+		}
+
+	}
+}
+
+func jump(currentIndex *int, operator string, value int) {
+	if operator == "+" {
+		*currentIndex = *currentIndex + value
+	}
+
+	if operator == "-" {
+		*currentIndex = *currentIndex - value
+	}
+}
+
+func updateAccumulator(acc *int, operator string, value int) {
+	if operator == "+" {
+		*acc = *acc + value
+	}
+
+	if operator == "-" {
+		*acc = *acc - value
+	}
+}
+
+func parseCommands(inputs []string) []command {
+
+	var commands []command
+
+	for index, input := range inputs {
+
+		value, _ := strconv.Atoi(input[5:])
+
+		commands = append(commands, command{
+			id:       index,
+			name:     input[0:3],
+			operator: input[4:5],
+			value:    value,
+		})
+	}
+
+	return commands
 }
