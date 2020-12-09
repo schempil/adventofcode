a303c2a2c1e2f3e758b745553d82bf48353b9839
Philipp Schemel <philippschemel@live.com>
2020-12-09 14:45:27 +0100

    Solve day9 part1

--- 2020/day9/day9.go
+++ 2020/day9/day9.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +14,55 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 9 - Part 1:", inputs[0])
+	sequence := parseInputsToSequence(inputs)
+
+	fmt.Println("Solution Day 9 - Part 1:", checkSequenceWithPreambleUntilError(sequence, 25))
 	fmt.Println("Solution Day 9 - Part 2:")
+}
+
+func parseInputsToSequence(inputs []string) []int {
+
+	var sequence []int
+
+	for _, input := range inputs {
+		parsedInput, _ := strconv.Atoi(input)
+		sequence = append(sequence, parsedInput)
+	}
+
+	return sequence
+}
+
+func checkSequenceWithPreambleUntilError(sequence []int, preambleCount int) int {
+
+	for index, _ := range sequence {
+		if index+preambleCount >= len(sequence) {
+			break
+		}
+
+		preamble := sequence[index : index+preambleCount]
+		target := sequence[index+preambleCount]
+
+		canBe := canBeSummedUp(preamble, target)
+
+		if canBe == false {
+			return target
+		}
+	}
+
+	return -1
+}
+
+func canBeSummedUp(sequence []int, target int) bool {
+
+	canBeSummedUp := false
+
+	for index, valueOne := range sequence {
+		for _, valueTwo := range sequence[index+1:] {
+			if target == valueOne+valueTwo {
+				canBeSummedUp = true
+			}
+		}
+	}
+
+	return canBeSummedUp
 }
