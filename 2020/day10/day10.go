34b702130ca1292a034c8ed37316d0a99788f9aa
Philipp Schemel <philippschemel@live.com>
2020-12-10 09:39:37 +0100

    Solve day10 part1

--- 2020/day10/day10.go
+++ 2020/day10/day10.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +15,51 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 10 - Part 1:", inputs[0])
+	fmt.Println("Solution Day 10 - Part 1:", countDifferences(parseInputsToSequence(inputs)))
 	fmt.Println("Solution Day 10 - Part 2:")
+}
+
+func parseInputsToSequence(inputs []string) []int {
+
+	var sequence []int
+
+	for _, input := range inputs {
+		parsedInput, _ := strconv.Atoi(input)
+		sequence = append(sequence, parsedInput)
+	}
+
+	return sequence
+}
+
+func countDifferences(inputs []int) int {
+
+	diffOneSum := 0
+	diffThreeSum := 1 //One because of the builtIn Adapter
+
+	sort.Ints(inputs)
+
+	for index, _ := range inputs {
+
+		var diff int
+
+		if index == 0 {
+			diff = inputs[index]
+		}
+
+		if index > 0 {
+			diff = inputs[index] - inputs[index-1]
+		}
+
+		if index+1 <= len(inputs) {
+			if diff == 1 {
+				diffOneSum++
+			}
+
+			if diff == 3 {
+				diffThreeSum++
+			}
+		}
+	}
+
+	return diffOneSum * diffThreeSum
 }
