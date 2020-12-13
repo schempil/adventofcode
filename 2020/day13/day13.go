a67ae2cd3ce7e98d3d7cfab6b502ed2394e4e722
Philipp Schemel <philippschemel@live.com>
2020-12-13 11:27:28 +0100

    Solve day13 part1

--- 2020/day13/day13.go
+++ 2020/day13/day13.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +14,34 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 13 - Part 1:", inputs[0])
+	earliest, _ := strconv.Atoi(inputs[0])
+	availableLines := strings.Split(inputs[1], ",")
+
+	fmt.Println("Solution Day 13 - Part 1:", getLineWithLeastWaitingTime(earliest, availableLines))
 	fmt.Println("Solution Day 13 - Part 2:")
+}
+
+func getLineWithLeastWaitingTime(earliest int, lines []string) int {
+
+	var waitingTime int
+	var lineWithLeastWaitingTime int
+
+	for index, line := range lines {
+
+		if line == "x" {
+			continue
+		}
+
+		currentLine, _ := strconv.Atoi(line)
+
+		currentWaitingTime := currentLine - (earliest % currentLine)
+
+		if index == 0 || currentWaitingTime < waitingTime {
+			waitingTime = currentWaitingTime
+			lineWithLeastWaitingTime = currentLine
+		}
+
+	}
+
+	return waitingTime * lineWithLeastWaitingTime
 }
