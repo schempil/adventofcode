a7f88e5abe693276461568a28fa4d31e8baefade
Philipp Schemel <philippschemel@live.com>
2020-12-15 13:46:04 +0100

    Remove all solutions every time

--- 2020/main.go
+++ 2020/main.go
@@ -1,20 +1,7 @@
 package main
 
 import (
-	"day1"
-	"day10"
-	"day11"
-	"day13"
-	"day14"
 	"day15"
-	"day2"
-	"day3"
-	"day4"
-	"day5"
-	"day6"
-	"day7"
-	"day8"
-	"day9"
 	"fmt"
 )
 
@@ -22,20 +9,6 @@ func main() {
 
 	fmt.Println("Hello AventOfCode 2020")
 
-	day1.Solve()
-	day2.Solve()
-	day3.Solve()
-	day4.Solve()
-	day5.Solve()
-	day6.Solve()
-	day7.Solve()
-	day8.Solve()
-	day9.Solve()
-	day10.Solve()
-	day11.Solve()
-
-	day13.Solve()
-	day14.Solve()
 	day15.Solve()
 
 }
