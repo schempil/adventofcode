66dc3f3d30efc9e55da1ed00c550e1b6e0b56992
Philipp Schemel <philippschemel@live.com>
2020-12-15 11:07:36 +0100

    Start solving of day15 part1

--- 2020/day15/day15.go
+++ 2020/day15/day15.go
@@ -11,8 +11,15 @@ func Solve() {
 	absolutePath, _ := filepath.Abs("./day15/input.txt")
 	content, _ := ioutil.ReadFile(absolutePath)
 	text := string(content)
-	inputs := strings.Split(text, "\n")
+	inputs := strings.Split(text, ",")
 
-	fmt.Println("Solution Day 15 - Part 1:", inputs)
+	fmt.Println("Solution Day 15 - Part 1:", get2020thSpokenNumber(inputs))
 	fmt.Println("Solution Day 15 - Part 2:")
+}
+
+func get2020thSpokenNumber(inputs []string) string {
+
+	var spoken []int
+
+	return "Not solved yet :("
 }
