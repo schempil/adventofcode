85f9f0b6bc022b6c9753474443111bfdacff3705
Philipp Schemel <philippschemel@live.com>
2020-12-18 01:54:35 +0100

    Solve day17 part2

--- 2020/day17/day17.go
+++ 2020/day17/day17.go
@@ -14,52 +14,47 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 17 - Part 1:", startBootProcessCycles(inputs, 6))
-	fmt.Println("Solution Day 17 - Part 2:")
+	fmt.Println("Solution Day 17 - Part 1:", startBootProcessCycles(inputs, 6, false))
+	fmt.Println("Solution Day 17 - Part 2:", startBootProcessCycles(inputs, 6, true))
 }
 
 type coordinate struct {
 	x int
 	y int
 	z int
+	w int
 }
 
 type coordinates []coordinate
 
 type dimension map[coordinate]string
 
-func startBootProcessCycles(inputs []string, cycles int) int {
+func startBootProcessCycles(inputs []string, cycles int, fourthDimension bool) int {
 
 	pocketDimension := make(dimension)
-	pocketDimension.initializeFromInput(inputs)
+	pocketDimension.initializeFromInput(inputs, fourthDimension)
 
 	for i := 0; i < cycles; i++ {
-		pocketDimension = pocketDimension.doCycle()
+		pocketDimension = pocketDimension.doCycle(fourthDimension)
 	}
 
 	return pocketDimension.getActiveStateCount()
 }
 
-func (pocketDimension dimension) doCycle() dimension {
+func (pocketDimension dimension) doCycle(fourthDimension bool) dimension {
 
 	newPocketDimension := make(dimension)
 
-	for coordinate, value := range pocketDimension {
-		newPocketDimension[coordinate] = value
-	}
-
-	for coord, _ := range pocketDimension {
-
-		neighbors := coord.getNeighbors()
+	for coord, value := range pocketDimension {
+		newPocketDimension[coord] = value
+		neighbors := coord.getNeighbors(fourthDimension)
 		pocketDimension.addMissingNeighbors(neighbors)
 	}
 
 	for coord, _ := range pocketDimension {
 
-		neighbors := coord.getNeighbors()
-
+		neighbors := coord.getNeighbors(fourthDimension)
 		activeNeighbors := neighbors.getActiveNeighbors(pocketDimension)
-
 		newPocketDimension[coord] = pocketDimension.getUpdatedStateOfCoordinate(coord, activeNeighbors)
 	}
 
@@ -130,59 +125,70 @@ func (pocketDimension dimension) getStateOfCoordinate(coord coordinate) string {
 	return "."
 }
 
-func (coord coordinate) getNeighbors() coordinates {
-	directions := []coordinate{
-		{x: 0, y: 0, z: 1},
-		{x: 0, y: 0, z: -1},
-		{x: 0, y: 1, z: 0},
-		{x: 0, y: 1, z: 1},
-		{x: 0, y: 1, z: -1},
-		{x: 0, y: -1, z: 0},
-		{x: 0, y: -1, z: 1},
-		{x: 0, y: -1, z: -1},
-		{x: 1, y: 0, z: 0},
-		{x: 1, y: 0, z: 1},
-		{x: 1, y: 0, z: -1},
-		{x: 1, y: 1, z: 0},
-		{x: 1, y: 1, z: 1},
-		{x: 1, y: 1, z: -1},
-		{x: 1, y: -1, z: 0},
-		{x: 1, y: -1, z: 1},
-		{x: 1, y: -1, z: -1},
-		{x: -1, y: 0, z: 0},
-		{x: -1, y: 0, z: 1},
-		{x: -1, y: 0, z: -1},
-		{x: -1, y: 1, z: 0},
-		{x: -1, y: 1, z: 1},
-		{x: -1, y: 1, z: -1},
-		{x: -1, y: -1, z: 0},
-		{x: -1, y: -1, z: 1},
-		{x: -1, y: -1, z: -1},
+func (coord coordinate) getNeighbors(fourthDimension bool) coordinates {
+
+	var fromW int
+	var toW int
+
+	if fourthDimension {
+		fromW = -1
+		toW = 1
 	}
 
 	var neighbors []coordinate
 
-	for _, direction := range directions {
-		neighbors = append(neighbors, coordinate{
-			x: coord.x + direction.x,
-			y: coord.y + direction.y,
-			z: coord.z + direction.z,
-		})
+	for x := -1; x <= 1; x++ {
+		for y := -1; y <= 1; y++ {
+			for z := -1; z <= 1; z++ {
+				for w := fromW; w <= toW; w++ {
+					if x == 0 && y == 0 && z == 0 && w == 0 {
+						continue
+					}
+
+					direction := coordinate{
+						x: x, y: y, z: z,
+					}
+
+					if fourthDimension {
+						direction.w = w
+					}
+
+					newCoordinate := coordinate{
+						x: coord.x + direction.x,
+						y: coord.y + direction.y,
+						z: coord.z + direction.z,
+					}
+
+					if fourthDimension {
+						newCoordinate.w = coord.w + direction.w
+					}
+
+					neighbors = append(neighbors, newCoordinate)
+
+				}
+			}
+		}
 	}
 
 	return neighbors
 }
 
-func (pocketDimension dimension) initializeFromInput(inputs []string) {
+func (pocketDimension dimension) initializeFromInput(inputs []string, fourthDimension bool) {
 
 	for rowIndex, row := range inputs {
 		for colIndex, col := range row {
 
-			pocketDimension[coordinate{
+			newCoordinate := coordinate{
 				x: colIndex,
 				y: rowIndex,
 				z: 0,
-			}] = string(col)
+			}
+
+			if fourthDimension {
+				newCoordinate.w = 0
+			}
+
+			pocketDimension[newCoordinate] = string(col)
 		}
 	}
 }
