55b8973f220232e530f5bdf3554a76dd4297a548
Philipp Schemel <philippschemel@live.com>
2020-12-22 14:14:35 +0100

    Solve day22 part2

--- 2020/day22/day22.go
+++ 2020/day22/day22.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -15,27 +16,35 @@ func Solve() {
 	text := string(content)
 	inputs := strings.Split(text, "\n")
 
-	fmt.Println("Solution Day 22 - Part 1:", getWinningPlayersScore(inputs))
-	fmt.Println("Solution Day 22 - Part 2:")
+	//fmt.Println("Solution Day 22 - Part 1:", getWinningPlayersScore(inputs, playNormalGame))
+	fmt.Println("Solution Day 22 - Part 2:", getWinningPlayersScore(inputs, playRecursiveCombat))
 }
 
 type player struct {
 	cards []int
 }
 
-func getWinningPlayersScore(inputs []string) int {
+type round struct {
+	playerOne player
+	playerTwo player
+}
+
+type playFunction func(playerOne player, playerTwo player, history []round) (playerOneNew player, playerTwoNew player, earlyFinish bool)
+
+func getWinningPlayersScore(inputs []string, play playFunction) int {
 
 	playerOne, playerTwo := getPlayersFromInput(inputs)
-	playerOne, playerTwo = playGame(playerOne, playerTwo)
+
+	playerOne, playerTwo, _ = play(playerOne, playerTwo, []round{})
 
 	var winningPlayer *player
 
-	if len(playerOne.cards) > 0 {
-		winningPlayer = &playerOne
-	}
-
 	if len(playerTwo.cards) > 0 {
 		winningPlayer = &playerTwo
+	}
+
+	if len(playerOne.cards) > 0 {
+		winningPlayer = &playerOne
 	}
 
 	score := calculateScoreFromCards(winningPlayer.cards)
@@ -55,10 +64,88 @@ func calculateScoreFromCards(cards []int) int {
 	return score
 }
 
-func playGame(playerOne player, playerTwo player) (playerOneNew player, playerTwoNew player) {
+func didAlreadyOccurInHistory(playerOne player, playerTwo player, history []round) bool {
+
+	for _, entry := range history {
+		if reflect.DeepEqual(entry.playerOne.cards, playerOne.cards) &&
+			reflect.DeepEqual(entry.playerTwo.cards, playerTwo.cards) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func playRecursiveCombat(playerOne player, playerTwo player, history []round) (playerOneNew player, playerTwoNew player, earlyFinish bool) {
+
+	if didAlreadyOccurInHistory(playerOne, playerTwo, history) {
+		return playerOne, playerTwo, true
+	}
+
+	history = append(history, round{
+		playerOne: playerOne,
+		playerTwo: playerTwo,
+	})
 
 	if len(playerOne.cards) == 0 || len(playerTwo.cards) == 0 {
-		return playerOne, playerTwo
+		return playerOne, playerTwo, false
+	}
+
+	currentCardPlayerOne := playerOne.cards[0]
+	currentCardPlayerTwo := playerTwo.cards[0]
+
+	playerOneNew.cards = playerOne.cards[1:]
+	playerTwoNew.cards = playerTwo.cards[1:]
+
+	if currentCardPlayerOne <= len(playerOneNew.cards) && currentCardPlayerTwo <= len(playerTwoNew.cards) {
+
+		subPlayerOne, subPlayerTwo, subEarlyFinish := playSubGame(
+			append([]int{}, playerOneNew.cards[0:currentCardPlayerOne]...),
+			append([]int{}, playerTwoNew.cards[0:currentCardPlayerTwo]...))
+
+		if subEarlyFinish {
+			playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerOne)
+			playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerTwo)
+			return playRecursiveCombat(playerOneNew, playerTwoNew, history)
+		}
+
+		if len(subPlayerOne.cards) > 0 {
+			playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerOne)
+			playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerTwo)
+		}
+
+		if len(subPlayerTwo.cards) > 0 {
+			playerTwoNew.cards = append(playerTwoNew.cards, currentCardPlayerTwo)
+			playerTwoNew.cards = append(playerTwoNew.cards, currentCardPlayerOne)
+		}
+
+		return playRecursiveCombat(playerOneNew, playerTwoNew, history)
+	}
+
+	if currentCardPlayerOne > currentCardPlayerTwo {
+		playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerOne)
+		playerOneNew.cards = append(playerOneNew.cards, currentCardPlayerTwo)
+	}
+
+	if currentCardPlayerOne < currentCardPlayerTwo {
+		playerTwoNew.cards = append(playerTwoNew.cards, currentCardPlayerTwo)
+		playerTwoNew.cards = append(playerTwoNew.cards, currentCardPlayerOne)
+	}
+
+	return playRecursiveCombat(playerOneNew, playerTwoNew, history)
+}
+
+func playSubGame(playerOneCards []int, playerTwoCards []int) (subPlayerOne player, subPlayerTwo player, earlyFinish bool) {
+	subPlayerOne = player{cards: playerOneCards}
+	subPlayerTwo = player{cards: playerTwoCards}
+
+	return playRecursiveCombat(subPlayerOne, subPlayerTwo, []round{})
+}
+
+func playNormalGame(playerOne player, playerTwo player, history []round) (playerOneNew player, playerTwoNew player, earlyFinish bool) {
+
+	if len(playerOne.cards) == 0 || len(playerTwo.cards) == 0 {
+		return playerOne, playerTwo, false
 	}
 
 	currentCardPlayerOne := playerOne.cards[0]
@@ -77,7 +164,7 @@ func playGame(playerOne player, playerTwo player) (playerOneNew player, playerTw
 		playerTwoNew.cards = append(playerTwoNew.cards, currentCardPlayerOne)
 	}
 
-	return playGame(playerOneNew, playerTwoNew)
+	return playNormalGame(playerOneNew, playerTwoNew, history)
 }
 
 func getPlayersFromInput(inputs []string) (playerOne player, playerTwo player) {
